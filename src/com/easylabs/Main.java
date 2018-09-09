5c019169642c4f0824f65049a84740a47a96cae2
Heimdallmk <elenanaboka13@gmail.com>
2018-09-09 14:13:26 +0300

    Общая информация о студенте.

---
+++ src/com/easylabs/Main.java
@@ -0,0 +1,14 @@
+package com.easylabs;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("ФИО- Набока Дмитрий Олегович");
+        System.out.print("Студент группы-АТ-161,бюджет");
+        System.out.print("очная форма обучения");
+        System.out.print("   курс-3\n");
+        System.out.println(" кол-во прожитых лет-20");
+        System.out.println("специальность- Автоматизация и\tкомпьютерно-интегрированные\b системы");
+
+    }
+}
